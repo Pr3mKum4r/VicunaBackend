c648a4785646dddd8a8df9847b5479674734f039
Prem Kumar Singh <premkumar.singh2021@vitstudent.ac.in>
2024-02-20 22:37:07 +0530

    fix: getOrderId fixed

--- src/app/api/payments/orders/route.ts
+++ src/app/api/payments/orders/route.ts
@@ -2,32 +2,41 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
+interface RequestBody{
+    amount: string;
+    name: string;
+    email: string;
+    number: string;
+    address: string;
+    product: string;
+    profile_name: string;
+}
+
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    let body:RequestBody = JSON.parse(await new Response(req.body).text());
     try {
         var instance = new Razorpay({ key_id: process.env.KEY_ID || "", key_secret: process.env.KEY_SECRET || "" })
         var options = {
-            "amount": parseInt(req.body.amount) * 100,
+            "amount": parseInt(body.amount) * 100,
             "currency": "INR",
             "receipt": "TXN" + Date.now(),
             "notes": {
-                key1: req.body.name,
-                key2: req.body.email,
-                key3: req.body.number,
-                key4: req.body.address,
-                key5: req.body.product,
-                key6: req.body.profile_name,
-                key7: req.body.amount,
+                key1: body.name,
+                key2: body.email,
+                key3: body.number,
+                key4: body.address,
+                key5: body.product,
+                key6: body.profile_name,
+                key7: body.amount,
             }
         };
-
-        console.log(req.body.amount);
         
         const order = await instance.orders.create(options);
         if (order) {
-            res.json(order.id); // Use res.json to send back the order ID
+            return NextResponse.json(order.id); // Use res.json to send back the order ID
         } else {
             // Handle the case where order is not created
-            res.status(500).json({ message: "Order creation failed" });
+            return NextResponse.json({ message: "Order creation failed" });
         }
     } catch (err) {
         console.log(err);
